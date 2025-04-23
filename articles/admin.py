eaa9994a8b1d3af9567f428641e062d3b2f0e6b3
garfmik <zulfiaaganova@gmail.com>
2025-04-23 23:01:35 +0500

    Article create

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from articles.models import Article
 
+class ArticleAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
 # Register your models here.
 admin.site.register(Article)
--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from articles.models import Article
-
+from . import forms
 
 def article_list(request):
     articles = Article.objects.all().order_by('date')
@@ -10,3 +10,18 @@ def article_list(request):
 def article_item(request, slug):
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_item.html', {'article': article})
+
+@login_required(login_url='accounts:login')
+def article_create(request):
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
+            return redirect('homepage')
+            #return redirect('articles:article_list')
+    else:
+        form = forms.CreateArticle()
+
+    return render(request, 'articles/article_create.html', {'form': form})
